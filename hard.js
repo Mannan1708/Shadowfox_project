2fcf3aa90d2993a41624fc2d2d6db33a4596ad49
Mannan1708 <mannanagrawal17@gmail.com>
2024-07-10 15:14:31 +0530

    Client project

---
+++ hard.js
@@ -0,0 +1,8 @@
+function next(){
+    document.getElementById("2nd").style.display="block";
+    document.getElementById("1st").style.display="none";
+}
+function prev(){
+    document.getElementById("2nd").style.display="none";
+    document.getElementById("1st").style.display="block";
+}
